6dd2e45c6d590635e3156fca1dde532af349308a
iSlammedMyKindle <108752129+iSlammedMyKindle@users.noreply.github.com>
2026-07-08 15:36:28 -0400

    Created TLC integration and spam detection!

--- index.ts
+++ index.ts
@@ -4,10 +4,69 @@
 import EventEmitter from "node:events";
 import page from "./src/index.html";
 import { type efType, type effectDTO, effect } from "./src/types/effect.types";
+import { argv } from "node:process";
+import { address } from "./config.json";
 
 // Purely to store sessions based on hash. We don't need to per-say, but important to track in case something goes south, or at the very least debug & track ongoing things
 const clientMap = new Map<Bun.ServerWebSocket, (str: string) => void>();
 const emitter = new EventEmitter();
+const spamMap = new Map<string, SpamRecord>()
+
+// All users should only be allowed to only send a command every 30 seconds
+interface SpamRecord {
+    warned: boolean,
+    helpWarned: boolean,
+    playedSound: boolean,
+    gotHelp: boolean,
+}
+
+interface TlcDTO {
+    text?: string,
+    user?: string,
+    id?: string
+    accepted?: String[],
+    rejected?: String[]
+}
+
+function releaseSpamRecord(user: string) {
+    spamMap.delete(user);
+}
+
+function isSpamming(user: string, id: string, ws: WebSocket, isHelpCmd: boolean = false): boolean {
+
+    // Ensure the person isn't spamming, then emit the event!
+    const targetSpamRecord = spamMap.get(user);
+    const targetWarning = isHelpCmd ? targetSpamRecord?.helpWarned : targetSpamRecord?.warned;
+
+    if (targetSpamRecord) {
+
+        if (!isHelpCmd && !targetSpamRecord.playedSound) {
+            targetSpamRecord.playedSound = true;
+            return false;
+        }
+
+        if (isHelpCmd && !targetSpamRecord.gotHelp) {
+            targetSpamRecord.gotHelp = true;
+            return false;
+        }
+
+        // Prevent spam
+        if (targetWarning) return true;
+
+        targetSpamRecord[isHelpCmd ? "helpWarned" : "warned"] = true;
+
+        ws.send(JSON.stringify({
+            action: "message", text: `[can] Goin too fast compadre! Please wait 30 seconds per-sound effect!`, replyTo: id
+        }));
+
+        return true;
+    }
+
+    spamMap.set(user, { warned: false, helpWarned: false, playedSound: !isHelpCmd, gotHelp: isHelpCmd });
+    setTimeout(() => releaseSpamRecord(user), 30000);
+
+    return false;
+}
 
 const server = Bun.serve({
     routes: {
@@ -61,6 +120,50 @@ const wsServer = Bun.serve({
         },
     },
 });
+
+if (!argv.includes("--no-tlc")) {
+    // Twitch Listener Core will accept twitch commands, based on messages sent to the chat & the types initiated
+    try {
+        const tlcServer = new WebSocket(address);
+
+        // Request permissions
+        tlcServer.addEventListener("open", _evt => {
+            console.log("Connected to TLC! Requesting msg perm...");
+            tlcServer.send(JSON.stringify(["message"]));
+        });
+
+        tlcServer.addEventListener("message", (evt) => {
+            const data = JSON.parse(evt.data) as TlcDTO;
+
+            console.log("TLC", data);
+
+            // Look for exclamation points
+            if (data.accepted || (data.text || "")[0] != "!") return;
+
+            // If we got a bite, then identify if that's a command we parse
+            const cmd = data.text!.substring(1).split(" ")[0];
+            if (cmd == "can") {
+                if (isSpamming(data.user!, data.id!, tlcServer, true)) return;
+
+                tlcServer.send(JSON.stringify({
+                    action: "message", text: `[can] List of sounds: ${[...effect.values].map(e => "!" + e).join(" ")}`, replyTo: data.id
+                }));
+            }
+
+            // Run the command!
+            else if (effect.safeParse(cmd).success) {
+                if (isSpamming(data.user!, data.id!, tlcServer)) return;
+                emitter.emit("effect", cmd);
+                tlcServer.send(JSON.stringify({
+                    action: "message", text: `[can] Now sending "${cmd}" ...`, replyTo: data.id
+                }));
+            }
+        });
+    }
+    catch (e) {
+        console.error("Failed to connect to TLC!", e);
+    }
+}
 
 function wsOnEffect(ws: Bun.ServerWebSocket, effect: string) {
     ws.send(JSON.stringify({
