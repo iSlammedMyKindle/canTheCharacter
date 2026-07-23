401f1ecc8c359ea897b011f21617ab1358a4bfad
Zachary Mitchell <31785316+zachMitchell@users.noreply.github>
2026-07-22 20:17:07 -0400

    appended the ablity to connect to an external server

--- src/can-script.ts
+++ src/can-script.ts
@@ -204,7 +204,7 @@ const animations = {
     },
 }
 
-const wsConnection = new WebSocket("ws://localhost:9014");
+const wsConnection = new WebSocket(`ws://${new URL(window!.location.href).searchParams.get("host") || "localhost"}:9014`);
 wsConnection.addEventListener("message", (evt => {
     let data: effectDTO;
     console.log(evt.data);
